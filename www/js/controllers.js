0c09e83b072778fb8de056e769aafae60cfba914
Henrique Nogueira <hrqnogueira@gmail.com>
2016-05-22 00:53:12 -0300

    Finished register controller

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,38 +1,159 @@
-angular.module('app.controllers', [])
+angular.module('app.controllers', ['ngCordova'])
 
-.controller('registerCtrl', function($scope) {
+  .controller('registerCtrl', function (serverEndpoint, $scope, $state, $window, $ionicPlatform, $ionicLoading,
+                                        $ionicPopup, $cordovaDevice, $http, $timeout, Register, LocationService) {
+    $scope.currentDate = new Date();
+    $scope.shouldShowDelete = false;
+    $scope.totalWorkedToday = "Carregando...";
 
-})
+    $ionicPlatform.ready(function () {
+      loadRegisters = function () {
+        $scope.registers = [];
+        Register.get({uuid: $cordovaDevice.getDevice().uuid}, function (response) {
+          angular.forEach(JSON.parse(JSON.stringify(response)).data, function (register, idx) {
+            register.label = ['Entrada', 'Saída'][idx % 2];
+            register.time = new Date(register.time);
+            $scope.registers.push(register);
+          });
+        });
+      }
 
-.controller('reportCtrl', function($scope) {
+      addRegisterCallback = function (location) {
+        if (location != null) {
+          Register.save({uuid: $cordovaDevice.getDevice().uuid}, location, function() {}, function() {
+            $ionicPopup.alert({
+             title: 'Ocorreu um erro',
+             template: 'Não foi possível contactar o CatracaDigital. Assegure-se ' +
+             'de que seu celular está com WiFi habilitado e conectado ou que' +
+             'os serviços de dados do aparelho estejam ligados e tente novamente.'
+           });
+          });
+        }
+        else {
+          $ionicPopup.alert({
+           title: 'Ocorreu um erro',
+           template: 'Não foi possível determinar sua localização. Favor habilitar ' +
+           'os serviços de localização do aparelho e tentar novamente.'
+         });
+        }
+        loadRegisters();
+        $ionicLoading.hide();
+      }
 
-})
+      addRegisterInit = function () {
+        $ionicLoading.show({template: 'Marcando o ponto'});
+        LocationService.getLocation(true, addRegisterCallback);
+      }
 
-.controller('historyCtrl', function($scope) {
+      $scope.showRegisterConfirm = function () {
+        $ionicPopup.confirm({
+          title: 'Registrar ponto',
+          template: 'Deseja registrar ponto neste momento?'
+        }).then(function (res) {
+          if (res) {
+            addRegisterInit();
+          }
+        });
+      };
 
-})
+      $scope.removeRegister = function (register, index) {
+        $ionicPopup.confirm({
+          title: 'Remover registro',
+          template: 'Deseja remover o registro?'
+        }).then(function (res) {
+          if (res) {
+            $ionicLoading.show({template: 'Removendo a marcação'});
+            $http({
+              method: 'DELETE',
+              url: serverEndpoint + "/delete_register/" + register.pk
+            }).then(function successCallback(response) {
+              loadRegisters();
+              $ionicLoading.hide();
+            }, function errorCallback(response) {
+              console.log("Could not remove entry.");
+            });
+          }
+        });
+      }
 
-.controller('infoCtrl', function($scope) {
+      getDuration = function (millis) {
+        var seconds = Math.floor(millis / 1000);
+        var minutes = Math.floor(seconds / 60);
+        seconds %= 60;
+        var hours = Math.floor(minutes / 60);
+        minutes %= 60;
 
-})
+        if (seconds < 10) seconds = "0" + seconds;
+        if (minutes < 10) minutes = "0" + minutes;
+        if (hours < 10)  hours = "0" + hours;
 
-.controller('signUpCtrl', function($scope) {
+        return hours + ":" + minutes + ":" + seconds;
+    }
 
-})
+      tickClock = function() {
+        registers = angular.copy($scope.registers);
 
-.controller('startCtrl', function($scope, $ionicLoading, $state, serverConfig) {
+        if (registers.length % 2 == 1) {
+          var nowRegister = new Date();
+          nowRegister.setHours(nowRegister.getHours() - 3);
+          registers.push({time: nowRegister});
+        }
 
-  $ionicLoading.show({template: 'Carregando...'});
+        var sum = 0;
+        for (var i = 1; i < registers.length; i += 2) {
+            sum += (new Date(registers[i].time) - new Date(registers[i - 1].time));
+        }
 
-  $scope.hide = function(){
-    $ionicLoading.hide().then(function(){
-       console.log("The loading indicator is now hidden");
+        $scope.totalWorkedToday = getDuration(sum);
+        $timeout(tickClock, 1000);
+      }
+
+      loadRegisters();
+      tickClock();
+    });
+  })
+
+  .controller('reportCtrl', function ($scope) {
+    
+  })
+
+  .controller('historyCtrl', function ($scope) {
+
+  })
+
+  .controller('infoCtrl', function ($scope, $ionicPlatform, $cordovaDevice, Employee) {
+    $ionicPlatform.ready(function () {
+      var device = $cordovaDevice.getDevice();
+      var uuid = device.uuid.toUpperCase();
+
+      $scope.employee = Employee.get({uuid: uuid}, function (employee) {
+        $scope.company = employee.company;
+        employee.admission_date = new Date(employee.admission_date);
+      });
+    });
+  })
+
+  .controller('signUpCtrl', function ($scope, $ionicPlatform, $cordovaDevice) {
+    $ionicPlatform.ready(function () {
+      var device = $cordovaDevice.getDevice();
+      $scope.uuid = device.uuid.toUpperCase();
+    });
+  })
+
+  .controller('startCtrl', function ($scope, $ionicLoading, $ionicPlatform, $state, $cordovaDevice, EmployeeService) {
+
+    $ionicPlatform.ready(function () {
+      $ionicLoading.show({template: 'Carregando...'});
+
+      var device = $cordovaDevice.getDevice();
+      EmployeeService.isRegistered(device.uuid, function () {
+        $ionicLoading.hide();
+        $state.go("catracaTabController.register");
+      }, function () {
+        $ionicLoading.hide();
+        $state.go("signUp");
+      });
 
     });
-  };
 
-  $scope.show();
-  setTimeout($scope.hide, 3000);
-  $state.go("signUp");
-
-})
+  })
--- www/js/services.js
+++ www/js/services.js
@@ -1,16 +1,49 @@
-angular.module('app.services', [])
+angular.module('app.services', ['ngResource'])
 
-.factory('BlankFactory', [function(){
+.factory('LocationService', ["$q","$cordovaGeolocation", function($q, $cordovaGeolocation) {
+  var latLong = null;
+  var getLocation = function(refresh, callback) {
+    var deferred = $q.defer();
+    var posOptions = {timeout: 10000, enableHighAccuracy: true};
+    if( latLong === null || refresh ) {
+      $cordovaGeolocation
+        .getCurrentPosition(posOptions)
+        .then(function(pos) {
+          latLong = {
+            'latitude' : pos.coords.latitude,
+            'longitude' : pos.coords.longitude
+          }
+          callback(latLong);
+          deferred.resolve(latLong);
+        }, function(error) {
+          latLong = null;
+          deferred.reject('Failed to Get Lat Long');
+          callback(latLong);
+      });
+    }
+    else {
+        deferred.resolve(latLong);
+    }
+    return deferred.promise;
+  };
 
+  return {
+      getLocation : getLocation
+  }
 }])
 
-.service('BlankService', [function(){
-
+.factory('Employee', ["serverEndpoint", "$resource", function(serverEndpoint, $resource) {
+  return $resource(serverEndpoint + "/employee/:uuid");
 }])
 
-.factory('SignUpService', [function ($http, serverEndpoint) {
+.factory('Register', ["serverEndpoint", "$resource",
+  function(serverEndpoint, $resource) {
+    return $resource(serverEndpoint + "/register/:uuid");
+}])
 
-
-
+.service('EmployeeService', ["serverEndpoint", "$http", function(serverEndpoint, $http) {
+  this.isRegistered = function(uuid, isRegistered, notFound) {
+    $http.get(serverEndpoint + "/employee/" + uuid).then(isRegistered, notFound);
+  }
 }]);
 
