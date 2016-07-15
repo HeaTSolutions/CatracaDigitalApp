605246111b2df7878b3f98535e0da4b9db25921c
Henrique Nogueira <hrqnogueira@gmail.com>
2016-07-15 14:46:39 -0300

    Updated Server endpoint

--- www/js/app.js
+++ www/js/app.js
@@ -1,7 +1,7 @@
 angular
   .module('app', ['ionic', 'app.controllers', 'app.routes', 'app.services', 'app.directives'])
-  //.constant("serverEndpoint", "http://catracadigital.com.br/api")
-  .constant("serverEndpoint", "http://192.168.0.104:8000/api")
+  .constant("serverEndpoint", "http://catracadigital.com.br/api")
+  //.constant("serverEndpoint", "http://192.168.0.104:8000/api")
   .run(function ($ionicPlatform) {
     $ionicPlatform.ready(function () {
       if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
